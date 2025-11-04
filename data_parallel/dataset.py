d890acfb2e0154a0ecedc5a136642781fcc49895
ppaleja <46587956+ppaleja@users.noreply.github.com>
2025-11-04 13:42:54 -0800

    Implement data partitioning and update .gitignore
    Add extensive Python .gitignore entries. Implement Partition,
    DataPartitioner, and partition_dataset in data_parallel/dataset.py with
    deterministic shuffling and proper DataLoader wrapping. Clean up
    docstrings, class definitions, and remove an unused import.

--- data_parallel/dataset.py
+++ data_parallel/dataset.py
@@ -1,52 +1,63 @@
 from random import Random
-import torch
+
 from torch.utils.data import DataLoader
 import torch.distributed as dist
 
 
-class Partition():
+class Partition:
     def __init__(self, data, index):
         self.data = data
         self.index = index
-    
+
     def __len__(self):
         return len(self.index)
-    
+
     def __getitem__(self, index):
-        '''Given index, get the data according to the partitioned index'''
+        """Given index, get the data according to the partitioned index"""
         # BEGIN ASSIGN5_1_1
-        raise NotImplementedError("Data Parallel Not Implemented Yet")
+        return self.data[self.index[index]]
         # END ASSIGN5_1_1
 
-class DataPartitioner():
+
+class DataPartitioner:
     def __init__(self, data, sizes=[0.7, 0.2, 0.1], seed=1234):
         self.data = data
         self.partitions = []
         rng = Random()
         rng.seed(seed)
-        ''' Create indices for different partitions
+        """ Create indices for different partitions
         1. Create indices and use `rng` to shuffle indices
         2. Create different partitions of indices according to `sizes` and store in `self.partitions`
-        '''
+        """
         # BEGIN ASSIGN5_1_1
-        raise NotImplementedError("Data Parallel Not Implemented Yet")
+        indices = list(range(len(data)))
+        # 1. Create indices and use `rng` to shuffle indices
+        rng.shuffle(indices)
+        # 2. Create different partitions of indices according to `sizes` and store in `self.partitions`
+        partition_sizes = [int(s * len(data)) for s in sizes]
+        partition_sizes[-1] = len(data) - sum(partition_sizes[:-1])
+        self.partitions = [
+            Partition(data, indices[i : i + size])
+            for i, size in enumerate(partition_sizes)
+        ]
         # END ASSIGN5_1_1
 
     def use(self, partition):
-        ''' Return a simple dataset class `Partiton` by original data and partitioned indices
+        """Return a simple dataset class `Partiton` by original data and partitioned indices
 
         Just one line of code. Think it simply.
-        '''
+        """
         # BEGIN ASSIGN5_1_1
-        raise NotImplementedError("Data Parallel Not Implemented Yet")
+        return Partition(self.data, self.partitions[partition].index)
         # END ASSIGN5_1_1
 
+
 def partition_dataset(rank, world_size, dataset, batch_size=128, collate_fn=None):
-    """ Partitioning training dataset of the Machine Translation
+    """Partitioning training dataset of the Machine Translation
 
     Returns:
         DataLoader: partitioned dataloader
-    
+
     Hint:
     1. Calculate the partitioned batch size
     2. Create a partitioner class `DataPartitioner` with dataset and the list of partitioned sizes
@@ -54,5 +65,15 @@ def partition_dataset(rank, world_size, dataset, batch_size=128, collate_fn=None
     4. Wrap the dataset with `DataLoader`, remember to customize the `collate_fn`
     """
     # BEGIN ASSIGN5_1
-    raise NotImplementedError("Data Parallel Not Implemented Yet")
+    # 1. Calculate the partitioned batch size
+    partitioned_batch_size = batch_size // world_size
+    # 2. Create a partitioner class `DataPartitioner` with dataset and the list of partitioned sizes
+    partitioner = DataPartitioner(dataset, [partitioned_batch_size] * world_size)
+    # 3. Get the current partition dataset given `rank`, use the `use` function in DataPartitioner
+    partition = partitioner.use(rank)
+    # 4. Wrap the dataset with `DataLoader`, remember to customize the `collate_fn`
+    dataloader = DataLoader(
+        partition, batch_size=partitioned_batch_size, collate_fn=collate_fn
+    )
+    return dataloader
     # END ASSIGN5_1
